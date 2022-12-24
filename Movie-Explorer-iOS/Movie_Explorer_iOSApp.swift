2b4228a3361b61efa6ba02cb1f07a7a5a1a17fe8
Oli Chen <olichen97@gmail.com>
2022-12-24 00:48:51 -0800

    Initial Commit

---
+++ Movie-Explorer-iOS/Movie_Explorer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_Explorer_iOSApp.swift
+//  Movie-Explorer-iOS
+//
+//  Created by Oli Chen on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Movie_Explorer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
